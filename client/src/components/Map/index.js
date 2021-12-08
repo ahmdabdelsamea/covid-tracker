cf08afb20d0efa37e39139446709aff2eb644b89
Ahmed Abdelsamea <94923716+ahmdabdelsamea@users.noreply.github.com>
2021-12-08 16:40:15 +0200

    popup done

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Map as LeafletMap, TileLayer } from 'react-leaflet';
 import { showDataOnMap } from '../../utils';
 import './map.css';
 
 function Map({ countries, casesType, center, zoom }) {
+	const mapRef = useRef(3);
+
 	return (
 		<div className='map'>
-			<LeafletMap center={center} zoom={zoom}>
+			<LeafletMap center={center} zoom={zoom} ref={mapRef}>
 				<TileLayer
 					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
