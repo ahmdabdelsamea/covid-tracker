fbbdbb838f660c947761d2a6b1a0459b52a1b346
Ahmed Abdelsamea <94923716+ahmdabdelsamea@users.noreply.github.com>
2021-12-11 11:16:29 +0200

    fix empty data array buh at linegraph

--- src/components/LineGraph/index.js
+++ src/components/LineGraph/index.js
@@ -48,7 +48,7 @@ const options = {
 	},
 };
 
-const buildChartData = (data, casesType) => {
+const buildChartData = (data = [], casesType) => {
 	let chartData = [];
 	let lastDataPoint;
 	for (let date in data.cases) {
